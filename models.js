e3ed92c8d4b1da8e60456397ac62233b0f616bd7
or <127608254+orazerad@users.noreply.github.com>
2024-11-25 07:37:31 +0200

    added 1 more destination and added flights to the model section

--- models.js
+++ models.js
@@ -4,5 +4,17 @@ const destinations = [
     {
         id: 1,
         name: 'London'
+    },
+    {
+        id: 2,
+        name: 'Tel Aviv'
     }
 ]
+
+const flights = [
+    {
+      flightNumber: 'OA101',
+      departureCode: 'TLV',
+      arrivalCode: 'LHR', 
+    },
+  ];
