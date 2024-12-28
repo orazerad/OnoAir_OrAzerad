279a1a4a7b1a8d89979fc86a6d885f673d4773b5
or <127608254+orazerad@users.noreply.github.com>
2024-12-28 23:46:13 +0200

    added flight service

--- onoair/src/app/app.module.ts
+++ onoair/src/app/app.module.ts
@@ -7,6 +7,16 @@ import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -22,6 +32,32 @@ import { FooterComponent } from './components/layout/footer/footer.component';
       AppRoutingModule,
       BrowserAnimationsModule,
       MaterialModule
+    ],
+    exports: [
+        MatCardModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
+
+
+
+@NgModule({
+    declarations: [
+      AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      // ... other components
+    ],
+    imports: [
+      BrowserModule,
+      AppRoutingModule,
+      BrowserAnimationsModule,
+      MaterialModule
+    ],
+    exports: [
+        MatCardModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- onoair/src/app/services/flight.service.ts
+++ onoair/src/app/services/flight.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Flight } from '../models/flight.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FlightService {
+  private flights: Flight[] = [
+    // Your 10 flights from Part A
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  getAll(): Observable<Flight[]> {
+    return of(this.flights);
+  }
+
+  getById(flightNumber: string): Observable<Flight | undefined> {
+    return of(this.flights.find(f => f.flightNumber === flightNumber));
+  }
+
+  getLastMinuteFlights(): Observable<Flight[]> {
+    const today = new Date();
+    const nextWeek = new Date();
+    nextWeek.setDate(today.getDate() + 7);
+
+    return of(this.flights.filter(flight => {
+      const flightDate = new Date(flight.departureDateTime);
+      return flightDate >= today && flightDate <= nextWeek;
+    }));
+  }
 }
