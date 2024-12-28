b0a8cf6ab371cb909dd00772289f6f6c1d23c9ee
or <127608254+orazerad@users.noreply.github.com>
2024-12-28 23:29:59 +0200

    added flight model

---
+++ onoair/src/app/models/flight.model.ts
@@ -0,0 +1,9 @@
+export interface Flight {
+    flightNumber: string;
+    departureCode: string;
+    arrivalCode: string;
+    departureDateTime: string;
+    arrivalDateTime: string;
+    totalSeats: number;
+    availableSeats: number;
+}
