e8afaa8ddb5d7207d1a67e8fb651f8ec269d1dbd
or <127608254+orazerad@users.noreply.github.com>
2024-12-28 23:34:46 +0200

    refactored the header components

--- onoair/src/app/components/layout/header/header.component.ts
+++ onoair/src/app/components/layout/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
 
+  navigateHome() {
+    this.router.navigate(['/']);
+  }
 }
