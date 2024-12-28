d8a929bdca4dc5c80444de92d4e62c498c4c0407
or <127608254+orazerad@users.noreply.github.com>
2024-12-29 00:43:03 +0200

    added the managed flights functionality

--- onoair/src/app/components/admin/manage-flights/manage-flights.component.ts
+++ onoair/src/app/components/admin/manage-flights/manage-flights.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Flight } from '../../../models/flight.model';
+import { FlightService } from '../../../services/flight.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage-flights',
-  imports: [],
   templateUrl: './manage-flights.component.html',
-  styleUrl: './manage-flights.component.css'
+  styleUrls: ['./manage-flights.component.css']
 })
-export class ManageFlightsComponent {
+export class ManageFlightsComponent implements OnInit {
+  flights: Flight[] = [];
+  displayedColumns: string[] = [
+    'flightNumber', 
+    'departureCode', 
+    'arrivalCode', 
+    'departureDateTime', 
+    'arrivalDateTime', 
+    'totalSeats', 
+    'availableSeats',
+    'actions'
+  ];
 
+  constructor(
+    private flightService: FlightService,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.loadFlights();
+  }
+
+  loadFlights() {
+    this.flightService.getAll().subscribe(flights => {
+      this.flights = flights;
+    });
+  }
+
+  viewFlight(flightNumber: string) {
+    // Will be implemented in Part C
+    console.log('Viewing flight:', flightNumber);
+  }
 }
