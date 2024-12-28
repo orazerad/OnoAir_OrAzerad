5037439a94f50c1acd594fad94f13638d6fa1d98
or <127608254+orazerad@users.noreply.github.com>
2024-12-29 00:14:20 +0200

    refactor the system to work

--- onoair/src/app/components/layout/footer/footer.component.ts
+++ onoair/src/app/components/layout/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
-})
-export class FooterComponent {
+  styleUrls: ['./footer.component.css'],
+  imports: [MatIconModule, MatButtonModule, RouterModule]
 
-}
+})
+export class FooterComponent {}
--- onoair/src/app/components/layout/header/header.component.ts
+++ onoair/src/app/components/layout/header/header.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  imports: [MatToolbarModule, MatListModule, RouterModule]
 })
 export class HeaderComponent {
   constructor(private router: Router) {}
