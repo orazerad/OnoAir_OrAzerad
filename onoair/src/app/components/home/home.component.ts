8c798de46283e25af062a050c2f21682ec558f33
or <127608254+orazerad@users.noreply.github.com>
2024-12-28 23:43:49 +0200

    added home components ts

--- onoair/src/app/components/home/home.component.ts
+++ onoair/src/app/components/home/home.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Flight } from '../../models/flight.model';
+import { FlightService } from '../../services/flight.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  lastMinuteFlights: Flight[] = [];
+  allFlights: Flight[] = [];
+  displayedColumns: string[] = ['flightNumber', 'departure', 'arrival', 'departureTime', 'arrivalTime', 'seats', 'actions'];
 
+  constructor(private flightService: FlightService) {}
+
+  ngOnInit() {
+    this.flightService.getAll().subscribe(flights => {
+      this.allFlights = flights;
+      // Filter last minute flights (this week)
+      const today = new Date();
+      const nextWeek = new Date();
+      nextWeek.setDate(today.getDate() + 7);
+      
+      this.lastMinuteFlights = flights.filter(flight => {
+        const flightDate = new Date(flight.departureDateTime);
+        return flightDate >= today && flightDate <= nextWeek;
+      });
+    });
+  }
+
+  bookFlight(flightNumber: string) {
+    console.log('Booking flight:', flightNumber);
+  }
 }
