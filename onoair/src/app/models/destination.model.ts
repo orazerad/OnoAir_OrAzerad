1eea5fdb6027225185439ab4ad46fdc733071edd
or <127608254+orazerad@users.noreply.github.com>
2024-12-28 23:29:37 +0200

    added destination model

---
+++ onoair/src/app/models/destination.model.ts
@@ -0,0 +1,9 @@
+export interface Destination {
+    id: number;
+    name: string;
+    code: string;
+    airportName: string;
+    airportWebsite: string;
+    email: string;
+    imageUrl: string;
+}
