a6ae4e2801bbb4c7f94f449238a8eb1b71b51b73
or <127608254+orazerad@users.noreply.github.com>
2024-12-29 08:45:12 +0200

    converted anohter component to stand alone

--- onoair/src/app/components/admin/manage-flights/manage-flights.component.ts
+++ onoair/src/app/components/admin/manage-flights/manage-flights.component.ts
@@ -2,21 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { Flight } from '../../../models/flight.model';
 import { FlightService } from '../../../services/flight.service';
 import { Router } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {DatePipe} from '@angular/common';
+import {MatIcon} from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-manage-flights',
   templateUrl: './manage-flights.component.html',
-  styleUrls: ['./manage-flights.component.css']
+  styleUrls: ['./manage-flights.component.css'],
+  imports: [
+    MatCardModule,
+    MatTableModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    DatePipe,
+    MatIcon
+  ],
+  standalone: true
 })
 export class ManageFlightsComponent implements OnInit {
   flights: Flight[] = [];
   displayedColumns: string[] = [
-    'flightNumber', 
-    'departureCode', 
-    'arrivalCode', 
-    'departureDateTime', 
-    'arrivalDateTime', 
-    'totalSeats', 
+    'flightNumber',
+    'departureCode',
+    'arrivalCode',
+    'departureDateTime',
+    'arrivalDateTime',
+    'totalSeats',
     'availableSeats',
     'actions'
   ];
