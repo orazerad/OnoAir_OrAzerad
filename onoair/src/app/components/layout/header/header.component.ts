37120618e3c4c7b68b7911e45fcde84742d28364
or <127608254+orazerad@users.noreply.github.com>
2024-12-29 08:46:47 +0200

    refactored the header component from scratch

--- onoair/src/app/components/layout/header/header.component.ts
+++ onoair/src/app/components/layout/header/header.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
+// import { Router } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import {MatButton} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  imports: [MatToolbarModule, MatListModule, RouterModule]
+  // imports: [MatToolbarModule, MatListModule,],
+  imports: [CommonModule, MatToolbarModule, MatListModule, RouterModule, MatButton],
+  standalone: true
 })
 export class HeaderComponent {
   constructor(private router: Router) {}
 
   navigateHome() {
     this.router.navigate(['/']);
+  }
+
+  navigateManageDestinations() {
+    this.router.navigate(['/manage-destinations']);
   }
 }
