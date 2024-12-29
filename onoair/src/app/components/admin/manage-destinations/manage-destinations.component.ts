348f08e0d6eeb40183a6d28f37115ef12b02e99d
or <127608254+orazerad@users.noreply.github.com>
2024-12-29 08:44:39 +0200

    converted component to standalone

--- onoair/src/app/components/admin/manage-destinations/manage-destinations.component.ts
+++ onoair/src/app/components/admin/manage-destinations/manage-destinations.component.ts
@@ -2,12 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Destination } from '../../../models/destination.model';
 import { DestinationService } from '../../../services/destination.service';
 import { Router } from '@angular/router';
+import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+
 
 @Component({
   selector: 'app-manage-destinations',
   templateUrl: './manage-destinations.component.html',
-  styleUrls: ['./manage-destinations.component.css']
+  styleUrls: ['./manage-destinations.component.css'],
+  imports: [MatCardModule, MatTableModule, MatIconModule, MatButtonModule],
+  standalone: true
 })
+
 export class ManageDestinationsComponent implements OnInit {
   destinations: Destination[] = [];
   displayedColumns: string[] = ['code', 'name', 'airportName', 'airportWebsite', 'email', 'actions'];
